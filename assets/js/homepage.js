d85f3278a57e26d791d61f874da3f74f5a992d7c
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-14 00:17:38 -0500

    add formSubmitHandler

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,36 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-}
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.prentDefault();
+    console.log(event);
+};
+
+//get value from input element
+var username = nameInputEl.value.trim();
+
+if (username){
+    getUserRepos(username);
+    nameInputEl.value = "";
+} else {
+    alert("Please enter a GitHub username");
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
 getUserRepos();
+}
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
